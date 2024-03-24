69541b3171dbc4516dbc4f3e5e090452c4def078
OneOfOne <oneofone@gmail.com>
2024-03-24 14:23:21 -0500

    clean up, get it to compile

--- build.rs
+++ build.rs
@@ -1,45 +1,45 @@
 extern crate bindgen;
 
-use std::env;
-use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+	Command::new("sh")
+		.arg("-e")
+		.arg("[ -f lib/Makefile ] ||  git submodule update --init --recursive")
+		.current_dir("lib")
+		.status()
+		.expect("failed to configure liburing");
 
-    Command::new("cp")
-        .arg("-r")
-        .arg("lib")
-        .arg(out_dir.clone())
-        .status()
-        .expect("copy liburing to out_dir");
-    Command::new("make")
-        .arg("liburing.a")
-        .current_dir(format!("{}/lib/src", out_dir.clone()))
-        .env("CFLAGS", "-fPIC")
-        .status()
-        .expect("failed to build liburing.a");
+	Command::new("sh")
+		.arg("configure")
+		.current_dir("lib")
+		.status()
+		.expect("failed to build liburin");
+	
+	Command::new("make")
+		.arg("liburing.a")
+		.current_dir("lib/src")
+		.env("CFLAGS", "-fPIC -O2 -fno-plt")
+		.status()
+		.expect("failed to build liburing.a");
 
-    // Tell cargo to tell rustc to link the system liburing
-    // shared library.
-    println!("cargo:rustc-link-lib=static=uring");
-    println!("cargo:rerun-if-changed=wrapper.h");
-    println!("cargo:rustc-link-search=native={}/lib/src", out_dir.clone());
+	println!("cargo:rustc-link-lib=static=uring");
+	println!("cargo:rerun-if-changed=wrapper.h");
+	println!("cargo:rustc-link-search=native=lib/src");
 
-    // Generate bindings
-    let bindings = bindgen::Builder::default()
-        .whitelist_function("__io_uring.*")
-        .whitelist_function("io_uring.*")
-        .whitelist_var("IORING.*")
-        .whitelist_var("IOSQE.*")
-        .whitelist_type("io_uring.*")
-        .header("wrapper.h")
-        .generate()
-        .expect("Unable to generate bindings");
+	// Generate bindings
+	let bindings = bindgen::Builder::default()
+		.allowlist_function("__io_uring.*")
+		.allowlist_function("io_uring.*")
+		.allowlist_var("IORING.*")
+		.allowlist_var("IOSQE.*")
+		.allowlist_type("io_uring.*")
+		.header("wrapper.h")
+		.use_core()
+		.prepend_enum_name(false)
+		.disable_name_namespacing()
+		.generate()
+		.expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(out_dir);
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+	bindings.write_to_file("src/raw/bindings.rs").expect("Couldn't write bindings!");
 }
--- tests/test_read_file.rs
+++ tests/test_read_file.rs
@@ -4,75 +4,68 @@ use std::mem;
 use std::os::unix::io::AsRawFd;
 
 use libc::off_t;
-use liburing::*;
+use uringer::raw::*;
 
 const QUEUE_DEPTH: u32 = 4;
 const READ_SIZE: usize = 128;
 
 #[test]
 fn test_io_uring_read_file() {
-    let mut ring = unsafe {
-        let mut s = mem::MaybeUninit::<io_uring>::uninit();
-        let ret = io_uring_queue_init(QUEUE_DEPTH, s.as_mut_ptr(), 0);
-        if ret < 0 {
-            panic!("io_uring_queue_init: {:?}", Error::from_raw_os_error(ret));
-        }
-        s.assume_init()
-    };
+	let mut ring = unsafe {
+		let mut s = mem::MaybeUninit::<io_uring>::uninit();
+		let ret = io_uring_queue_init(QUEUE_DEPTH, s.as_mut_ptr(), 0);
+		if ret < 0 {
+			panic!("io_uring_queue_init: {:?}", Error::from_raw_os_error(ret));
+		}
+		s.assume_init()
+	};
 
-    let file = File::open("/proc/self/exe").unwrap();
+	let file = File::open("/proc/self/exe").unwrap();
 
-    let mut iovecs: Vec<libc::iovec> =
-        vec![unsafe { mem::zeroed() }; QUEUE_DEPTH as usize];
-    for iov in iovecs.iter_mut() {
-        let buf = unsafe {
-            let mut s = mem::MaybeUninit::<*mut libc::c_void>::uninit();
-            if libc::posix_memalign(s.as_mut_ptr(), 4096, READ_SIZE) != 0 {
-                panic!("can't allocate");
-            }
-            s.assume_init()
-        };
-        iov.iov_base = buf;
-        iov.iov_len = READ_SIZE;
-    }
+	let mut iovecs: Vec<libc::iovec> = vec![unsafe { mem::zeroed() }; QUEUE_DEPTH as usize];
+	for iov in iovecs.iter_mut() {
+		let buf = unsafe {
+			let mut s = mem::MaybeUninit::<*mut libc::c_void>::uninit();
+			if libc::posix_memalign(s.as_mut_ptr(), 4096, READ_SIZE) != 0 {
+				panic!("can't allocate");
+			}
+			s.assume_init()
+		};
+		iov.iov_base = buf;
+		iov.iov_len = READ_SIZE;
+	}
 
-    let mut offset: usize = 0;
-    for i in 0.. {
-        let sqe = unsafe { io_uring_get_sqe(&mut ring) };
-        if sqe == std::ptr::null_mut() {
-            break;
-        }
-        unsafe {
-            io_uring_prep_readv(
-                sqe,
-                file.as_raw_fd(),
-                &mut iovecs[i],
-                1,
-                offset as off_t,
-            )
-        };
-        offset += READ_SIZE;
-    }
-    let ret = unsafe { io_uring_submit(&mut ring) };
-    if ret < 0 {
-        panic!("io_uring_submit: {:?}", Error::from_raw_os_error(ret));
-    }
+	let mut offset: usize = 0;
 
-    let mut cqe: *mut io_uring_cqe = unsafe { std::mem::zeroed() };
-    // let mut done = 0;
-    let pending = ret;
-    for _ in 0..pending {
-        let ret = unsafe { io_uring_wait_cqe(&mut ring, &mut cqe) };
-        if ret < 0 {
-            panic!("io_uring_wait_cqe: {:?}", Error::from_raw_os_error(ret));
-        }
-        // done += 1;
-        if unsafe { (*cqe).res } != READ_SIZE as i32 {
-            eprintln!("(*cqe).res = {}", unsafe { (*cqe).res });
-        }
-        unsafe { io_uring_cqe_seen(&mut ring, cqe) };
-    }
+	#[allow(clippy::needless_range_loop)]
+	for i in 0.. {
+		let sqe = unsafe { io_uring_get_sqe(&mut ring) };
+		if sqe.is_null() {
+			break;
+		}
+		unsafe { io_uring_prep_readv(sqe, file.as_raw_fd(), &iovecs[i], 1, offset as off_t) };
+		offset += READ_SIZE;
+	}
+	let ret = unsafe { io_uring_submit(&mut ring) };
+	if ret < 0 {
+		panic!("io_uring_submit: {:?}", Error::from_raw_os_error(ret));
+	}
 
-    // println!("Submitted={}, completed={}", pending, done);
-    unsafe { io_uring_queue_exit(&mut ring) };
+	let mut cqe: *mut io_uring_cqe = unsafe { std::mem::zeroed() };
+	// let mut done = 0;
+	let pending = ret;
+	for _ in 0..pending {
+		let ret = unsafe { io_uring_wait_cqe(&mut ring, &mut cqe) };
+		if ret < 0 {
+			panic!("io_uring_wait_cqe: {:?}", Error::from_raw_os_error(ret));
+		}
+		// done += 1;
+		if unsafe { (*cqe).res } != READ_SIZE as i32 {
+			eprintln!("(*cqe).res = {}", unsafe { (*cqe).res });
+		}
+		unsafe { io_uring_cqe_seen(&mut ring, cqe) };
+	}
+
+	// println!("Submitted={}, completed={}", pending, done);
+	unsafe { io_uring_queue_exit(&mut ring) };
 }
